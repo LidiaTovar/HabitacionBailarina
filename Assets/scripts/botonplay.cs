4c9620a17fa271f32ef427daca9484fdc1d7963c
marina <73647333+Vodizea@users.noreply.github.com>
2021-01-28 19:11:44 +0100

    botonpause
    conseguio, pero falta alomehor conseguir que se quede en sitio de pausa

---
+++ Assets/scripts/botonplay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class botonplay : MonoBehaviour
+{
+    public bool animation_bool;
+
+    //Start is blablabla
+    void Start()
+    {
+
+    }
+
+    //Update is called blablabla
+    void Update()
+    {
+       
+    }
+    public void playAnimation()
+    {
+        if (animation_bool == true)
+        {
+            gameObject.GetComponent<Animator>().Play("cubo");
+            animation_bool = false;
+
+        }
+        else
+        {
+            gameObject.GetComponent<Animator>().Play("Idle");
+            animation_bool = true;
+
+        }
+    }
+
+}
