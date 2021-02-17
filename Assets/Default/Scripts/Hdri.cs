2dba2c2344fcfad7dae47b8a9da7597acbcd564d
DavidSalva11 <davidsalvatierracastro@gmail.com>
2021-02-17 18:22:57 +0100

    hecho scroipt

---
+++ Assets/Default/Scripts/Hdri.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hdri : MonoBehaviour
+{
+    public Material mat1;
+    public Material mat2;
+    public Material mat3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
