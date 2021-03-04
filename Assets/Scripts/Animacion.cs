ce1825a9ff01fb9a3c94f50f8a1417716289feb5
LidiaTovar <72567217+LidiaTovar@users.noreply.github.com>
2021-03-04 12:59:52 +0100

    ui

---
+++ Assets/Scripts/Animacion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animacion : MonoBehaviour
+{
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Animation>().Play("Baile");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
